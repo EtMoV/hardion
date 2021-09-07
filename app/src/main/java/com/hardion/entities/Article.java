917fca88df713f19854344be26d72f2ff0da5c81
EtMoV <motaraetiennepro@gmail.com>
2021-09-07 14:27:36 +0200

    cleaning

--- app/src/main/java/com/hardion/entities/Article.java
+++ app/src/main/java/com/hardion/entities/Article.java
@@ -11,12 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
-
-import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id", "title" }) })
